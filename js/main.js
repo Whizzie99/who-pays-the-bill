ca2b31d2a0c30d5937a74d8334200208fa19ae29
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2020-11-29 00:28:58 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+new Vue({
+    el: '#app',
+    data:{
+        state: true,
+        inputName: '',
+        names: [],
+        showError: false,
+        results: ''
+    },
+
+    methods:{
+        addNameToList(){
+            if(this.validate(this.inputName)){
+                this.names.push(this.inputName);
+                this.inputName = '';
+                this.showError = false;
+                console.log('its valid');
+            } else{
+                this.showError = true
+                console.log('not valid');
+            }
+        },
+
+        removeName(index){
+            this.names.splice(index, 1);
+        },
+
+        validate(value){
+            if(value !== ''){
+                return true;
+            } else{
+                return false;
+            }
+        },
+
+        showResults(){
+            let rand = this.names[Math.floor(Math.random() * this.names.length)];
+            this.result = rand;
+            this.state = false;
+        },
+
+        resetApp(){
+            this.state = true;
+            this.names = [];
+            this.result = ''
+        }
+    }
+})
